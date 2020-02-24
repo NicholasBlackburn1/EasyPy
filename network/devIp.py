9178aa025cb794db7ae7244904b1b61beac3bb0a
nicholas blackburn <nickblackburn02@gmail.com>
2020-02-24 10:35:03 -0500

    added User ip connection and ip address

--- network/devIp.py
+++ network/devIp.py
@@ -1,9 +1,13 @@
 # Grabs Local ip for dev info
 # graps Public Ip for ip
 
+import requests
 import socket
+
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
+ip = requests.get('https://checkip.amazonaws.com').text.strip()
 
+print("Your public Ip" + ip)
 print("Your Computer Name is:" + hostname)
 print("Your Computer IP Address is:" + IPAddr)
