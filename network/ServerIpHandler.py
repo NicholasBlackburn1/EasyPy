fb1675b037a4d98819132e165a3be1916a6b8c1c
nicholas blackburn <nickblackburn02@gmail.com>
2020-02-25 10:02:10 -0500

    added port to json and added udp port creation

--- network/ServerIpHandler.py
+++ network/ServerIpHandler.py
@@ -22,14 +22,21 @@ usage:
 # Logging for Ips
 logging.basicConfig(filename='Ip.log', level=logging.DEBUG)
 
-# reads Loaded Json
+# reads Loaded Json Config
 
-io = StringIO()
-read = []
+online = []
+offline = []
+port = []
+
+# Sets Server Ip and Port
+server_ip = ''
+server_port = ''
 
 
 def readConfig():
-    global read
+    global online
+    global offline
+    global server_port
 
     logging.info("config loading")
 
@@ -38,20 +45,31 @@ def readConfig():
         data = json.load(read_it)
 
     # Grabs mode from json
-    read = data['mode']
+    online = data['online']
+    offline = data['offline']
+    port = data['port']
 
-    logging.info("\n" + read)
+    logging.info("Loaded server mode \n")
 
 
 def getServerIP():
 
-    global server_local
-    global server_public
+    global online
+    global offline
+    global server_ip
+    global server_port
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    # Sets local ip adress for server
+    IPAddr = s.getsockname()[0]
+    s.close()
 
     hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
+
     ip = requests.get('https://checkip.amazonaws.com').text.strip()
 
+    # Logs out Your Server Public Ip Server Host name and Your Local Network Ip
     logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     logging.debug("Your publicIp" + ip)
     logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -60,16 +78,34 @@ def getServerIP():
     logging.debug("Your Computer IP Address is:" + IPAddr)
     logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    if ():
-        logging.debug("User Set Server to Be Local ")
+    if (online == True):
+        logging.info("User Set Server to Be ONLINE Mode")
+        # Sets Server Ip To your Public Ip address
+        server_ip = ip
+        logging.info("Set Server Ip to " + server_ip)
 
-    if ():
-        logging.debug("User Set Server to Be Public")
+        # Starts Server With Your Public ip
+        startServer(server_ip, server_port)
+
+    elif (offline == True):
+        logging.info("User Set Server to Be OFFLINE Mode")
+
+        logging.debug(
+            "OFFLINE MODE / INSECURE MODE ACTIVE ALL Checks Have been Disabled ")
+        # Sets Server Ip To your Local Ip address
+        server_ip = IPAddr
+        logging.info("Set Server Ip to " + server_ip)
+
+        startServer(server_ip, server_port)
 
     else:
         print("set server in Online / offline mode to continue")
         logging.fatal("set server in Online / offline mode to continue")
 
 
-def startServer(args):
-    print("")
+def startServer(server_ip, server_port):
+    print("server ip" + server_ip + "port\n" + server_port)
+
+    # creates server Socket and
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((server_ip, server_port))
