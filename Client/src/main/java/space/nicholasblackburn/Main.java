59d4b01138ff04db13140680cfb5dc8f3d6de867
nicholas blackburn <nickblackburn02@gmail.com>
2020-02-27 11:22:12 -0500

    Added /Started  Java Client for Server

---
+++ Client/src/main/java/space/nicholasblackburn/Main.java
@@ -0,0 +1,14 @@
+/**
+ * This Class Is Where All The Main Program Runs from
+ * 
+ * @author Nicholas Blackburn
+ */
+package space.nicholasblackburn;
+
+public class Main {
+
+    public static void fxInit() {
+
+    }
+
+}
